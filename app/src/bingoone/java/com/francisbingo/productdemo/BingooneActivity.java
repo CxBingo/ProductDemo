82b39d381fce7d3f1d5e09c9de2f754671c24e29
v_cuixu <v_cuixu@baidu.com>
2018-02-13 09:47:27 +0800

    first commit

---
+++ app/src/bingoone/java/com/francisbingo/productdemo/BingooneActivity.java
@@ -0,0 +1,13 @@
+package com.francisbingo.productdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BingooneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bingoone);
+    }
+}
---
+++ app/src/main/java/com/francisbingo/productdemo/MainActivity.java
@@ -0,0 +1,23 @@
+package com.francisbingo.productdemo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.textView);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,BingooneActivity.class));
+            }
+        });
+    }
+}
